9ea5eadc27a1496c10ca3e6be0eeb7d11dd5cda1
Epshtein Dmitry <letuver@yahoo.com>
2021-12-02 15:50:04 +0200

    remove old func

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const RandomNumber = Math.round(100 * Math.random());
 function checkNum(rNum){
     function getAnsw(){
         let Answ = prompt("Угадай число от 1 до 100");
-        console.log(Answ);
+         //console.log(Answ);
         if(isNaN(Answ))
         {
             alert("Введите валидное число!");
@@ -15,20 +15,20 @@ function checkNum(rNum){
             Answ = +Answ.trim();
             if(Answ > rNum){
                 console.log("Загаданное число меньше");
-                console.log(Answ);
-                console.log(RandomNumber);
+               // console.log(Answ);
+                //console.log(RandomNumber);
                 getAnsw();
             }
             else if(Answ < rNum){
                 console.log("Загаданное число больше");
-                console.log(Answ);
-                console.log(RandomNumber);
+                //console.log(Answ);
+                //console.log(RandomNumber);
                 getAnsw();
             }
             else if(Answ == rNum){
                 console.log("Поздравляю, Вы угадали!!!");
-                console.log(Answ);
-                console.log(RandomNumber);
+                //console.log(Answ);
+                //console.log(RandomNumber);
             }
         }
         else{
@@ -39,21 +39,3 @@ function checkNum(rNum){
     getAnsw();
 }
 checkNum(RandomNumber);
-//
-
-function one(x){
-    function two(){
-        const a = +prompt("Enter a number: ");
-        x--;
-
-        console.log(a);
-        console.log(x);
-
-        if(x!=a){
-            two();
-        }
-    }
-    two();
-}
-
-//one(10);
